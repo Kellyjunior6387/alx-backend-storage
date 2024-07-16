c7355deec89669722f2f4ade430ab1306b7cb549
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-16 16:52:28 +0300

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -2,14 +2,14 @@
 """Module to provide nginx logs stored in mongodb"""
 from pymongo import MongoClient
 
-
-client = MongoClient()
-logs = client.logs.nginx
-method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-print("{} logs".format(logs.count_documents({})))
-print('Methods')
-for x in method:
-    print(" method {}: {}".format(x, logs.count_documents({'method': x})))
-print("{} status check".format(logs.count_documents({'method': 'GET',
-                                                     'path': '/status'}
-                                                    )))
+if __name__ == "__main__":
+    client = MongoClient()
+    logs = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(logs.count_documents({})))
+    print('Methods')
+    for x in method:
+        print(" method {}: {}".format(x, logs.count_documents({'method': x})))
+    print("{} status check".format(logs.count_documents({'method': 'GET',
+                                                        'path': '/status'}
+                                                        )))
