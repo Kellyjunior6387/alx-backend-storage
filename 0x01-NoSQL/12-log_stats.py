4f7ffd3a904f3964c6cd3acb442dca9e881bdc16
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-16 16:50:03 +0300

    task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to provide nginx logs stored in mongodb"""
+from pymongo import MongoClient
+
+
+client = MongoClient()
+logs = client.logs.nginx
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+print("{} logs".format(logs.count_documents({})))
+print('Methods')
+for x in method:
+    print(" method {}: {}".format(x, logs.count_documents({'method': x})))
+print("{} status check".format(logs.count_documents({'method': 'GET',
+                                                     'path': '/status'}
+                                                    )))
