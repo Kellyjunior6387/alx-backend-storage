f449536288f6d60c82342711f50814f33d98a7dd
Kellyjunior6387 <kellyjunior6386@gmail.com>
2024-07-16 16:59:51 +0300

    task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python3
-"""Module to provide nginx logs stored in mongodb"""
 from pymongo import MongoClient
 
+
+def log_stats():
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+    # Total number of logs
+    total = collection.count_documents({})
+    # Number of logs for each HTTP method
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_count = {method: collection.count_documents({"method": method})
+                    for method in methods}
+    # Number of logs with method GET and path /status
+    status_check = collection.count_documents({"method": "GET",
+                                               "path": "/status"})
+    # Display the results
+    print(f"{total} logs")
+    print("Methods:")
+    for method in methods:
+        print(f"\tmethod {method}: {method_count[method]}")
+    print(f"{status_check} status check")
+
+
 if __name__ == "__main__":
-    client = MongoClient()
-    logs = client.logs.nginx
-    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    print("{} logs".format(logs.count_documents({})))
-    print('Methods')
-    for x in method:
-        print("\tmethod {}: {}".format(x, logs.count_documents({'method': x})))
-    print("{} status check".format(logs.count_documents({'method': 'GET',
-                                                        'path': '/status'}
-                                                        )))
+    log_stats()
---
+++ 0x01-NoSQL/mine.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module to provide nginx logs stored in mongodb"""
+from pymongo import MongoClient
+
+if __name__ == "__main__":
+    client = MongoClient()
+    logs = client.logs.nginx
+    method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("{} logs".format(logs.count_documents({})))
+    print('Methods')
+    for x in method:
+        print("\tmethod {}: {}".format(x, logs.count_documents({'method': x})))
+    print("{} status check".format(logs.count_documents({'method': 'GET',
+                                                        'path': '/status'}
+                                                        )))
